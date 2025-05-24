10f2113bf495fee6ba5e3a7dadff3b072a203187
rinebob111185 <rinebob111185@gmail.com>
2025-05-24 23:33:50 +0000

    Rotates firebaseConfig apiKey; implements secrets handling

--- src/firebase/firebase.config.ts
+++ src/firebase/firebase.config.ts
@@ -1,13 +1,9 @@
 import { initializeApp, FirebaseApp } from "firebase/app";
+import { FIREBASE_CONFIG } from "../environments/secrets/secrets";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyB3Pr7nLxdN0Nt75Lnh8f6TEHY2eBkt6DQ",
-    authDomain: "alpha-vantage-proxy-api.firebaseapp.com",
-    projectId: "alpha-vantage-proxy-api",
-    storageBucket: "alpha-vantage-proxy-api.firebasestorage.app",
-    messagingSenderId: "29825344315",
-    appId: "1:29825344315:web:a4b3fdbe30b43e6a820d1e"
+    ...FIREBASE_CONFIG
 };
 
 // Initialize Firebase App
