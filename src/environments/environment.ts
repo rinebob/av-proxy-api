d3b2cbc7dfe2d0d642c45ce301c403aeb3190922
rinebob <rinebob111185@gmail.com>
2025-06-25 15:26:36 -0700

    Removes last vestige of old functions base url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,9 +5,5 @@ export const environment = {
   useEmulator: true,
   firebaseConfig: {
     ...FIREBASE_CONFIG
-  },
-  // Local emulator URL for all Cloud Functions
-  functionsBaseUrl: 'http://localhost:5001/alpha-vantage-proxy-api/us-central1',
-  // Production URL (will be used in environment.prod.ts)
-  // functionsBaseUrl: 'https://us-central1-alpha-vantage-proxy-api.cloudfunctions.net'
+  }
 };
