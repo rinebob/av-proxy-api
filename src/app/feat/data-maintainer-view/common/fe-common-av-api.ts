293cdfadea94ce4da3f030ee9ed55ef9fe9cb05e
rinebob <rinebob111185@gmail.com>
2025-07-08 11:24:07 -0700

    [AV UI] Updates alphaVangageApi deployed function URL

--- src/app/feat/data-maintainer-view/common/fe-common-av-api.ts
+++ src/app/feat/data-maintainer-view/common/fe-common-av-api.ts
@@ -4,7 +4,7 @@ import { AlphaVantageEndpoint } from '../../../common/fe-common-av';
 /**
  * Production URL for the Alpha Vantage API Gateway
  */
-const AV_GATEWAY_PROD_URL = 'https://alpha-vantage-gateway-lsluydmucq-uc.a.run.app';
+const AV_GATEWAY_PROD_URL = 'https://alphavantageapi-lsluydmucq-uc.a.run.app';
 
 /**
  * Development URL base for the Alpha Vantage API Gateway
