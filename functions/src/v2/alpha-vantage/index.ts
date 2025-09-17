d419c902b969797e25f3ac476ab2eb5d64526862
rinebob <rinebob111185@gmail.com>
2025-09-17 10:33:10 -0700

    cleanup (av api): Deletes now deprecated and unused av-daily-time-series-updater.ts; Removes imports where necessary

--- functions/src/v2/alpha-vantage/index.ts
+++ functions/src/v2/alpha-vantage/index.ts
@@ -2,10 +2,6 @@ export * from './handlers/alpha-vantage-base.handler';
 export * from './handlers/av-global-quote.handler';
 export * from './handlers/av-symbol-search.handler';
 export * from '../common/enums';
-export { 
-  updateDailyTimeSeries,
-  updateDailyTimeSeriesHandler 
-} from './data-refresher/av-daily-time-series-updater';
 
 // Remove the underscore from the function name then uncomment the next line to export it
 // export { updateAllDailyTimeSeriesBulk } from './data-refresher/av-daily-time-series-bulk-updater';
